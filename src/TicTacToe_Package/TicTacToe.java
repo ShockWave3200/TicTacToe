e5c149e9ae6152fd63b1466d48d30516aafdcd1c
ShockWave3200 <pa.collet@orange.fr>
2023-01-08 14:37:52 +0100

    affichage du jeu + confirmation du placement pour TicTacToe 2D

--- src/TicTacToe_Package/TicTacToe.java
+++ src/TicTacToe_Package/TicTacToe.java
@@ -1,37 +1,61 @@
 package TicTacToe_Package;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 public class TicTacToe {
-  private char[][] board;
-  private char currentPlayer;
+  private String[][] board;
+  private String currentPlayer;
   private Scanner scanner;
   
   public TicTacToe() {
-    board = new char[3][3];
-    currentPlayer = 'X';
+    board = new String[3][3];
+    currentPlayer = "X";
     scanner = new Scanner(System.in);
     
     // Initialisation du plateau
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
-        board[i][j] = ' ';
+        board[i][j] = " ";
       }
     }
+  }
+  
+  //Affichage du plateau
+  public void viewBoard(String[][] board) {
+	  System.out.println("   0  1  2");
+      for (int i = 0; i < 3; i++) {
+        System.out.print(i+" ");
+        for (int j = 0; j < 3; j++) {
+          System.out.print(" "+board[i][j] + " ");
+        }
+        System.out.println();
+      }
+  }
+  
+//Affichage du plateau pour confirmer l'action
+  public void confirmedPlay(String[][] board,int row,int col , String currentPlayer) {
+	  System.out.println("   0  1  2");
+      for (int i = 0; i < 3; i++) {
+        System.out.print(i+" ");
+        for (int j = 0; j < 3; j++) {
+        	if(i==row && j==col) {
+        		System.out.print(">"+currentPlayer+"<");
+        	}else {
+        		System.out.print(" "+board[i][j]+" ");
+        	}
+        }
+        System.out.println();
+      }
   }
   
   public void play() {
     // Boucle de jeu
     while (true) {
-      // Affichage du plateau
-      System.out.println("  0 1 2");
-      for (int i = 0; i < 3; i++) {
-        System.out.print(i + " ");
-        for (int j = 0; j < 3; j++) {
-          System.out.print(board[i][j] + " ");
-        }
-        System.out.println();
-      }
+      
+    
+    	//Affichage du plateau
+      viewBoard(board);
       
       // Demande de la prochaine position au joueur courant
       System.out.print("Entrez la ligne (0, 1, 2) pour " + currentPlayer + " : ");
@@ -39,9 +63,18 @@ public class TicTacToe {
       System.out.print("Entrez la colonne (0, 1, 2) pour " + currentPlayer + " : ");
       int col = scanner.nextInt();
       
+      
       // Mise à jour du plateau
-      if (board[row][col] == ' ') {
-        board[row][col] = currentPlayer;
+      if (board[row][col] == " ") {
+    	 confirmedPlay(board,row,col,currentPlayer);
+    	 System.out.print("Vous confirmez votre placement (O/N) : ");
+    	 char confirmed=scanner.next().charAt(0);
+    	 if(confirmed=='O') {
+    	     board[row][col] = currentPlayer;
+    	 }else {
+    		 continue;
+    	 }
+
       } else {
         System.out.println("Cette case est déjà occupée, veuillez en choisir une autre.");
         continue;
@@ -57,7 +90,7 @@ public class TicTacToe {
       boolean isDraw = true;
       for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-          if (board[i][j] == ' ') {
+          if (board[i][j] == " ") {
             isDraw = false;
             break;
           }
@@ -69,16 +102,16 @@ public class TicTacToe {
       }
       
       // Passage au joueur suivant
-      if (currentPlayer == 'X') {
-        currentPlayer = 'O';
+      if (currentPlayer == "X") {
+        currentPlayer = "O";
       } else {
-        currentPlayer = 'X';
+        currentPlayer = "X";
       }
     }
   }
   
   // Méthode pour vérifier si le joueur courant a gagné
-  private boolean checkWin(char player) {
+  private boolean checkWin(String player) {
     // Vérifie les lignes
     for (int i = 0; i < 3; i++) {
       if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
